323dc68c7f1295bb96c8a841b88978c5e15e7c7f
Goncalo Feliciano <goncaloaaf@gmail.com>
2024-09-02 12:34:59 +0100

    meio da demo

---
+++ demo_1/main.py
@@ -0,0 +1,107 @@
+
+# comnet 1 linha
+
+"""
+comnet
+varias
+linha
+
+"""
+
+
+# var
+
+
+nome = "valor"
+print(nome)
+
+idade = 10
+print(idade)
+
+idade2 = 10.5
+print(idade2)
+
+aprovado = True
+print(aprovado)
+
+'''
+nome = 182
+print(nome)
+'''
+
+# op var
+
+#soma=idade+idade2
+
+soma = idade + idade2
+print(soma)
+
+# concat var
+
+nome = "Carlos"
+print("nome: {nome}, idade: {idade} mais txt")
+print(f"nome:\t\"{nome}\", \ \nidade: {idade} mais txt")
+
+"""
+
+\n
+\t
+\\
+\"
+
+"""
+
+
+# cond
+    # if
+print("--------if------")
+idade = 30
+
+if idade >= 18:
+    print("Adulto")
+else:
+    print("nao Adulto")
+
+print("-------- if v2 ------")
+idade = 16
+if idade > 18:
+    print("Adulto")
+elif idade > 15:
+    print("teen")
+else:
+    print("kid")
+
+print("-------- if v3 ------")
+
+
+"""
+&& - and
+|| - or
+! - not
+
+"""
+v1 = 10
+v2 = 9
+
+if v1 == 10 and not v2 == 20:
+    print("ok")
+else:
+    print("nao ok")
+
+    # while
+
+
+    # match case
+
+
+# loops
+    # for
+    # while
+
+# arr (listas)
+
+# dict
+
+
+# funcs
+
