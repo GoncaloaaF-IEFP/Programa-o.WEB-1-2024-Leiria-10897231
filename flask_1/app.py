449b475f26b927ac538c215222a953799b876f43
Goncalo Feliciano <goncaloaaf@gmail.com>
2024-09-09 11:56:46 +0100

    flask template, passagem de param e errorhandler

--- flask_1/app.py
+++ flask_1/app.py
@@ -2,8 +2,8 @@ from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
+@app.route('/homepage')
+def index():
     return 'Hello World!'
 
 
@@ -11,9 +11,11 @@ def hello_world():
 def pag2():
     return 'Hello World!- pag 2'
 
+
 @app.route('/pag3/')
 def pag3():
     return f"Hello World, Desconhecido"
+
 
 @app.route('/pag3/<nome>')
 def pag3_nome(nome:str):
@@ -42,4 +44,41 @@ def template():
 
 @app.route('/template2')
 def template2():
-    return render_template("pag2.html")
+    return render_template("pag2.html",
+                           titulo = "Pagina 2",
+                           header = "a minha 2ª pagina Flask com dados ..."
+                           )
+
+@app.route('/template3')
+def template3():
+    return render_template("pag2.html",
+                           titulo = "Pagina 3",
+                           header = "Pagina 3"
+                           )
+
+
+@app.route('/template4/<nome>')
+def template4(nome:str):
+    return render_template("pag2.html",
+                           titulo = "Pagina 4",
+                           header = f"Temp4: Bem Vindo, {nome}"
+                           )
+
+@app.route('/template5/')
+def template5():
+    nome = request.args.get('nome')
+    return render_template("pag2.html",
+                           titulo = "Pagina 5",
+                           header = f"Temp5: Bem Vindo, {nome}"
+                           )
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    print(e)
+    return render_template("not_found.html"), 404
+
+
+
+
+
